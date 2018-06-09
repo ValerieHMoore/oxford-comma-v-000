74cdad662252983eac6bc82e4672e857c395bb9f
Valerie Moore <periwinkle17@outlook.com>
2018-06-09 18:42:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,13 @@
 def oxford_comma(array)
   if array.length == 1
   array[0]
-  elsif array.length == 2
+  if array.length == 2
   array[0] + " and " + array[1]
-  elsif array.length == 3
+  if array.length == 3
     array[0] + ", " + array[1] + "and " + array[2]
-  else
+  if array.length > 3
     last_element == ", and " + array.last
     array.each do |item|
       item + ", "
-  end
   end
 end
